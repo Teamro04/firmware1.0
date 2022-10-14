e970556609f689ec64f548669a31acc10e8cd803
astianmuchui <sebastianmuchui79@gmail.com>
2022-10-14 15:33:14 +0300

    functions

--- src/firmware/firmware.ino
+++ src/firmware/firmware.ino
@@ -49,3 +49,67 @@ void forward(enablePin,speed){
   
 };
 
+void backward(enablePin,speed){
+  // Speed in percentage [0->100]
+  float value = map(speed,0,100,0,255);
+  analogWrite(enablePin,value);
+
+  // Mechanics   
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  
+};
+
+void right(enablePin,int speed){
+  float value = map(speed,0,100,0,255);
+  analogWrite(enablePin,value);
+  //Differential steering
+  digitalWrite(in1,LOW); // First motor back
+  digitalWrite(in2,HIGH);
+
+  digitalWrite(in3,HIGH); // Second Motor Front
+  digitalWrite(in4,LOW);
+};
+
+void left(int speed){
+  void right(enablePin,int speed){
+  float value = map(speed,0,100,0,255);
+  analogWrite(enablePin,value);
+  //Differential steering
+  digitalWrite(in1,HIGH); // First motor back
+  digitalWrite(in2,LOW);
+
+  digitalWrite(in3,LOW); // Second Motor Front
+  digitalWrite(in4,HIGH);
+};
+
+void U_turn_Right(int speed){
+  float value = map(speed,0,100,0,255);
+  // Assuming that in1 and in2 are the right motors and in3 and in4 being left motors
+  motor_off(in1);
+  motor_off(in2);
+  backward(75);
+};
+
+void U_turn_Left(int speed){
+  float value = map(speed,0,100,0,255);
+  // Assuming that in1 and in2 are the right motors and in3 and in4 being left motors
+  motor_off(in3);
+  motor_off(in4);
+  backward(75);
+};
+// Read distance from ultrasonic sensor 
+float distance(int echo,int trigger){
+
+  digitalWrite(trigger,LOW);
+  delayMicroseconds(10);
+
+  digitalWrite(trigger,HIGH);
+
+  long dr = pulseIn(echo,HIGH);
+  long distance = dr*0.034/2;
+
+  return distance;
+};
+
+
