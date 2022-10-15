3cbf840c34abc845c8c7487fc1e416a6857f7d94
astianmuchui <sebastianmuchui79@gmail.com>
2022-10-15 08:03:54 +0300

    building

--- src/firmware/firmware.ino
+++ src/firmware/firmware.ino
@@ -11,15 +11,21 @@
 #define ultrasonic1_trigger 27 
 
 #define ultrasonic2_echo 28 // To be installed on the right side
-#define ultrasonnic2_trigger 29
+#define ultrasonic2_trigger 29
 
 #define ultrasonic3_echo 30 // To be installed on the left side
 #define ultrasonic3_trigger 31
 
 // Array to hold all the pins up there
-uint8_t pins[] = {IR1,IR2,IR3,ena,enb,in1,in2,in3,in4};
+uint8_t outputs[] = {IR1,IR2,IR3,ena,enb,in1,in2,in3,in4,ultrasonic1_trigger,ultrasonic2_trigger,ultrasonic3_trigger}; // Output pins
+uint8_t inputs[] = {ultrasonic1_echo,ultrasonic2_echo,ultrasonic3_echo}; // Input pins
 void setup(){
-
+  for(int pin = 0; pin<12; pin++){
+    pinMode(outputs[pin],OUTPUT);
+  }
+  for(int i = 0; i<3; i++){
+    pinMode(inputs[i],INPUT);
+  }
 };
 void loop(){
   /*
@@ -31,10 +37,34 @@ void loop(){
   float front_distance = distance(ultrasonic1_echo,ultrasonic1_trigger);
   float right_distance = distance(ultrasonic2_echo,ultrasonic2_trigger);
   float left_distance = distance(ultrasonic3_echo,ultrasonic3_trigger);
-  
+  float speed = 50;
   do{
-    forward( float speed);
-  }while();
+    forward(90);
+  }while(front_distance > 3);
+  
+  if(front_distance < 3){
+
+    if(right_distance <5){
+
+      left(50);
+      delay(1000);
+
+      do{
+        forward(90);
+      }while(front_distance > 3);
+
+    }
+    else if(left_distance < 5){
+
+      right(50);
+      delay(1000);
+
+      do{
+        forward(90);
+      }while(front_distance > 3);
+    
+    }
+  }
 };
 
 
