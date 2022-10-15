9603acb759be6e415a31156b66fdf7ef847253f5
astianmuchui <sebastianmuchui79@gmail.com>
2022-10-15 08:09:39 +0300

    update

--- push.cmd
+++ push.cmd
@@ -2,11 +2,5 @@ git pull git@github.com:astianmuchui/Autonomous-Robot.git &
 git status &
 git add . &
 git commit -m "update" &
+git push git@github.com:astianmuchui/Autonomous-Robot.git
 
-if(git push git@github.com:astianmuchui/Autonomous-Robot.git
-){
-    echo "Deployed Successfully"
-}
-else{
-    echo "Deploy Failed"
-}
